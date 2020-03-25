259c3c038be6a8d2c80c75c2db68f187d03f7615
Jordan Black <jblack530@gmail.com>
2020-03-25 16:08:49 -0700

    simple node api setup exercise

---
+++ complete-node-bootcamp/1-node-farm/starter/index.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+
+
+// #1 Blocking, synchronous way of doing this
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+
+// console.log(textIn);
+
+// const textOut = `This is what we know about the avocado: ${textIn}. \nCreated on: 
+// ${Date.now()}`;
+
+// fs.writeFileSync('./txt/output.txt', textOut)
+// console.log('File written!')
+
+// #2 Non - blocking, async way
+
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt', 'utf-8', (err,data3) => {
+//             console.log(data3);
+
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log('Your file has been written :)');
+//             })
+//         })
+//     })
+// });
+
+// console.log('Will read file!')
+
+// #3 Creating simple node.js server
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+
+const server = http.createServer((req, res) => {
+
+    const pathName = req.url;
+
+    if(pathName === '/overview' || pathName === '/') {
+        res.end('This is the OVERVIEW')
+    } else if (pathName === '/product') {
+        res.end('This is the PRODUCT')
+    } else if (pathName === '/api') {
+        res.writeHead(200, {'Content-type': 'application/json'})
+        res.end(data)
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'Hello-world'
+        });
+        res.end('<h1>Page not found!</h1>')
+    }
+    res.end('Hello from the server!');
+})
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000')
+});
