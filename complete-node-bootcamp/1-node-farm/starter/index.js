797f2168953447298199b9335b37e2caab720e87
Jordan Black <jblack530@gmail.com>
2020-03-26 17:17:31 -0700

    finished product template, back button link returns to overview page

--- complete-node-bootcamp/1-node-farm/starter/index.js
+++ complete-node-bootcamp/1-node-farm/starter/index.js
@@ -57,11 +57,18 @@ const replaceTemplate = (temp, product) => {
 }
 
 const server = http.createServer((req, res) => {
-
-    const pathName = req.url;
+    
+    const {query, pathname} = url.parse(req.url, true);
+    
+    
+    
+    
+    
+    
+    
     // Overview Page
 
-    if(pathName === '/overview' || pathName === '/') {
+    if(pathname === '/overview' || pathname === '/') {
 
         res.writeHead(200, { 'Content-type': 'text/html' })
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
@@ -71,11 +78,18 @@ const server = http.createServer((req, res) => {
         res.end(output);
 
         // Product Page
-    } else if (pathName === '/product') {
-        res.end('This is the PRODUCT')
+    } else if (pathname === '/product') {
+        res.writeHead(200, { 'Content-type': 'text/html' })
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product)
+
+
+        res.end(output)
+
+        
 
         // API
-    } else if (pathName === '/api') {
+    } else if (pathname === '/api') {
         res.writeHead(200, {'Content-type': 'application/json'})
         res.end(data)
 
