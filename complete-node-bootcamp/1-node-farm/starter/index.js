9302a59736226f6c74a2138465c1a7e6b187f1a0
Jordan Black <jblack530@gmail.com>
2020-03-26 17:22:05 -0700

    removed replaceTemplate function from index.js and made it into its own module that gets imported

--- complete-node-bootcamp/1-node-farm/starter/index.js
+++ complete-node-bootcamp/1-node-farm/starter/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const replaceTemplate = require('./modules/replaceTemplate');
 
 
 // #1 Blocking, synchronous way of doing this
@@ -42,19 +43,19 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 
 const dataObj = JSON.parse(data);
 
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
+// const replaceTemplate = (temp, product) => {
+//     let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+//     output = output.replace(/{%IMAGE%}/g, product.image);
+//     output = output.replace(/{%PRICE%}/g, product.price);
+//     output = output.replace(/{%QUANTITY%}/g, product.quantity);
+//     output = output.replace(/{%FROM%}/g, product.from);
+//     output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+//     output = output.replace(/{%DESCRIPTION%}/g, product.description);
+//     output = output.replace(/{%ID%}/g, product.id);
     
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
+//     if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+//     return output;
+// }
 
 const server = http.createServer((req, res) => {
     
