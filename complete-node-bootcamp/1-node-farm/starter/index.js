fb1ffc5b28bcb98d0411efb8f61b4f7f4fb2da00
Jordan Black <jblack530@gmail.com>
2020-03-26 17:09:37 -0700

    filling html templates, pt 2

--- complete-node-bootcamp/1-node-farm/starter/index.js
+++ complete-node-bootcamp/1-node-farm/starter/index.js
@@ -35,20 +35,52 @@ const url = require('url');
 // #3 Creating simple node.js server
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
 const dataObj = JSON.parse(data);
 
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
 
 const server = http.createServer((req, res) => {
 
     const pathName = req.url;
+    // Overview Page
 
     if(pathName === '/overview' || pathName === '/') {
-        res.end('This is the OVERVIEW')
+
+        res.writeHead(200, { 'Content-type': 'text/html' })
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+
+
+        res.end(output);
+
+        // Product Page
     } else if (pathName === '/product') {
         res.end('This is the PRODUCT')
+
+        // API
     } else if (pathName === '/api') {
         res.writeHead(200, {'Content-type': 'application/json'})
         res.end(data)
+
+
+        // Not Found
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
