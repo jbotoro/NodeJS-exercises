26c40f6dad4180118f8da32338f733cb4d691631
Jordan Black <jblack530@gmail.com>
2020-04-02 13:47:24 -0700

    added class example as best practice to exercise

--- complete-node-bootcamp/2-how-node-works/starter/events.js
+++ complete-node-bootcamp/2-how-node-works/starter/events.js
@@ -1,7 +1,16 @@
 const EventEmitter = require('events');
 
-const myEmitter = new EventEmitter();
 
+// Best practice to create class and inherit from class
+
+class Sales extends EventEmitter {
+    constructor() {
+        super();
+    }
+}
+// Like this
+
+const myEmitter = new Sales();
 
 myEmitter.on('newSale', () => {
     console.log('There was a new sale!');
@@ -15,3 +24,8 @@ myEmitter.on('newSale', stock => {
     console.log(`There are now ${stock} items left in stock.`);
 })
 myEmitter.emit('newSale', 9);
+
+//////////////////////////
+
+
+
