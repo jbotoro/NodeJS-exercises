d09392b4e3d054e8c1fa11b9ad119cae7c957134
Jordan Black <jblack530@gmail.com>
2020-04-02 13:54:24 -0700

    server events exercise node

--- complete-node-bootcamp/2-how-node-works/starter/events.js
+++ complete-node-bootcamp/2-how-node-works/starter/events.js
@@ -1,5 +1,5 @@
 const EventEmitter = require('events');
-
+const http = require('http');
 
 // Best practice to create class and inherit from class
 
@@ -29,3 +29,20 @@ myEmitter.emit('newSale', 9);
 
 
 
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+    console.log("Request received!");
+    console.log(req.url)
+    res.end('Request received')
+})
+server.on('request', (req, res) => {
+    console.log('Another request :)')
+})
+server.on('close', () => {
+    console.log("Server closed");
+})
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Waiting for requests...')
+})
