6dce9f8bf0c9675d398f15c6a26f532a75eaae4c
Jordan Black <jblack530@gmail.com>
2020-04-03 13:01:14 -0700

    node streams execrise pt 2

--- complete-node-bootcamp/2-how-node-works/starter/streams.js
+++ complete-node-bootcamp/2-how-node-works/starter/streams.js
@@ -7,9 +7,29 @@ server.on('request', (req,res) => {
     // Method is problematic is you have a large number of requests
     // or if the data you a sending back in the response is large
 
-    fs.readFile('test-file.txt', (err, data) => {
-        if (err) console.log(err);
-        res.end(data);
+    // fs.readFile('test-file.txt', (err, data) => {
+    //     if (err) console.log(err);
+    //     res.end(data);
+    // });
+
+
+    //Method 2 : Streams
+    // In method 1 all the data was read and saved into variable before being sent
+    // to the client, whereas with streams we send each "chunk" of data as its read
+    // instead of waiting for the whole file to be read
+
+    const readable = fs.createReadStream('test-file.txt');
+    readable.on('data', (chunk) => {
+        res.write(chunk);
+    });
+    // response end necessary when whole file has been read
+    readable.on('end', () => {
+        res.end();
+    });
+    readable.on('error', err => {
+        console.log(err);
+        res.status(500);
+        res.end('File not found!')
     });
 });
 
