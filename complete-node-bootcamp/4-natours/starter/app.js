30697969725c1cc8511b9cc1bdbed8601fb34bad
Jordan Black <jblack530@gmail.com>
2020-04-07 16:43:01 -0700

    post request exercise node js natours api

--- complete-node-bootcamp/4-natours/starter/app.js
+++ complete-node-bootcamp/4-natours/starter/app.js
@@ -5,6 +5,9 @@ const express = require('express');
 
 const app = express();
 
+//middleware: data from body added to request
+app.use(express.json());
+
 // app.get('/', (req,res) => {
 //     res
 //         .status(200)
@@ -27,6 +30,26 @@ app.get('/api/v1/tours', (req, res) => {
         data: {
             tours
         }
+    })
+})
+
+app.post('/api/v1/tours', (req,res) => {
+    
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body)
+
+    tours.push(newTour);
+    
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simple.json`, 
+        JSON.stringify(tours), 
+        err => {
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    tour: newTour
+                }
+        });
     })
 })
 
