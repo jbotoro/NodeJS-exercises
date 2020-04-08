0acc19949839ed65ffdb3bd0248f4395bae2d8f1
Jordan Black <jblack530@gmail.com>
2020-04-07 17:00:43 -0700

    patch requests natours api:

--- complete-node-bootcamp/4-natours/starter/app.js
+++ complete-node-bootcamp/4-natours/starter/app.js
@@ -75,6 +75,25 @@ app.post('/api/v1/tours', (req,res) => {
     })
 })
 
+
+app.patch('/api/v1/tours/:id', (req,res) => {
+
+    if(req.params.id * 1 > tours.length){
+        return res.status(404).json({
+            status: 'fail',
+            message: "Invalid ID"
+        })
+    }
+
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: 'Updated tour coming soooon ...'
+        }
+    })
+})
+
 const port = 3000;
 
 
